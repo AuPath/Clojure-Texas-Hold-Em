44fb2971d787f9a4102758abfaa83f5cfc8811dc
AuPath <marco.au.grassi98@protonmail.com>
2020-07-11 20:54:09 +0200

    Added hand value predicates and deck and card functions

--- src/poker/core.clj
+++ src/poker/core.clj
@@ -5,3 +5,77 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+;;; Numero massimo di giocatori è 6
+;;; Come quando fuori piove, cuori quadri fiori picche
+;;; A,K,Q,J,10,9,8,7,6,5
+;;; 14,13,12,11,10,9,8,7,6,5
+;;; C,Q,F,P
+;;; R,B
+
+(defn card-lowest-value
+  "Returns value of the card that has the lowest value."
+  [number-players]
+  (- 11 number-players))
+
+(defn card
+  "Returns a card with VALUE, SUIT."
+  [value suit]
+  {:value value
+   :suit suit
+   :color (if (or (= \C suit)
+                  (= \Q suit))
+            \R
+            \N)})
+
+(defn group-of-n?
+  [n hand]
+  (boolean
+   (some #(< n %)
+         (vals (frequencies (map :value hand))))))
+
+(defn pair?
+  [hand]
+  (group-of-n? 2 hand))
+
+(defn tris?
+  [hand]
+  (group-of-n? 3 hand))
+
+(defn poker?
+  [hand]
+  (group-of-n? 4 hand))
+
+(defn colore?
+  [hand]
+  (= 1 (count (group-by :suit hand))))
+
+(defn scala?
+  [hand]
+  (let [values (sort (map :value hand))]
+    (= values
+       (range (apply min values) (+ 1 (apply max values))))))
+
+(defn scala-colore?
+  [hand]
+  (and (scala? hand)
+       (colore? hand)))
+
+(defn full?
+  [hand]
+  (and (tris? hand)
+       (pair? hand)))
+
+(defn deck
+  "Returns an initial unshuffled deck to play with NUMBER-PLAYERS."
+  [number-players]
+  (for [suit '(\C \Q \F \P)
+        value (range (card-lowest-value number-players) 15)]
+    (card value suit)))
+  
+
+
+
+
+  
+  
