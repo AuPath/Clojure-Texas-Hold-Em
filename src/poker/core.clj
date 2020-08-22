df34f06ac3ea2499dc5bb68f6262ed2a88cdb9f2
AuPath <marco.au.grassi98@protonmail.com>
2020-08-22 21:39:34 +0200

    Removed deck set related functions. Added player and game generating

--- src/poker/core.clj
+++ src/poker/core.clj
@@ -32,7 +32,7 @@
 (defn group-of-n?
   [n hand]
   (boolean
-   (some #(< n %)
+   (some #{n}
          (vals (frequencies (map :value hand))))))
 
 (defn pair?
@@ -82,9 +82,9 @@
 (defn deck-generate
   "Returns an initial unshuffled deck to play with NUMBER-PLAYERS."
   [number-players]
-  (for [suit '(\C \Q \F \P)
-        value (range (card-lowest-value number-players) 15)]
-    (card value suit)))
+  (into [] (for [suit '(\C \Q \F \P)
+                 value (range (card-lowest-value number-players) 15)]
+             (card value suit))))
 
 ;; {:deck '(list of cards)
 ;;  :discarded-cards '(list of cards)
@@ -129,39 +129,29 @@
   [game]
   (:deck game))
 
-(defn deck-add-cards
-  "Adds CARDS to DECK."
-  [deck cards]
-  (vec (clojure.set/union (set deck)
-                         (set cards))))
-
-(defn deck-remove-cards
-  "Removes CARDS to DECK."
-  [deck cards]
-  (vec (clojure.set/difference (set deck)
-                              (set cards))))
-
-;; (defn draw-n-cards-from-deck
-;;   ""
-;;   [game player-id n]
-;;   (let [deck (:deck game)
-;;         cards-drawn (take n deck)
-;;         new-game (assoc game :deck (clojure.set/difference (set deck)
-;;                                                            (set cards-drawn)))]
-;;     (update-player-cards new-game player-id cards-drawn)))
-
-
 (def game-example {:deck (shuffle (deck-generate 4))
                          :pot 0
                    :players {1 {:hand nil, :money 0}
                              2 {:hand nil, :money 100}}})
 
-(defn cards-discarded
-  "Returns discarded cards."
-  [game]
-  (vec
-   (clojure.set/difference (set (:deck game))
-                           (set (apply merge (map :hand (vals (:players game))))))))
+(defn money-starting-amount
+  "Money for each player depending on n of players."
+  [n]
+  0)
+
+(defn player-generate
+  ""
+  ([hand money] {:hand hand, :money money})
+  ([] player-generate nil 0))
+
+(defn game-generate
+  "Generate a poker game for n players."
+  [n]
+  {:deck (shuffle (deck-generate n))
+   :pot 0
+   :players (zipmap (range 1 (+ 1 n))
+                    (repeat n (player-generate nil (money-starting-amount n))))
+   })
 
 (defn phase-blind
   "Removes BLIND amount of money from all players."
@@ -178,7 +168,7 @@
   [game player-id n]
   (let [deck (deck game)
         drawn-cards (take n deck)
-        updated-deck (deck-remove-cards deck drawn-cards)
+        updated-deck (drop n deck)
         new-gamestate (update-game-deck game updated-deck)]
     (update-game-player new-gamestate
                         player-id
@@ -186,7 +176,7 @@
                                       drawn-cards))))
 
 (defn phase-card-distribution
-  ""
+  "Gives 5 cards from deck to all player in GAME."
   [game]
   (reduce #(player-draw-n-cards-from-deck %1 %2 5)
           game
@@ -201,12 +191,87 @@
                                         (- amount)))
       (update-game-pot amount)))
 
+(defn value-inter-hand-type
+  ""
+  [hand]
+  (cond (scala-reale? hand) 8000000
+        (poker? hand) 7000000
+        (colore? hand) 6000000
+        (full? hand) 5000000
+        (scala? hand) 4000000
+        (tris? hand) 3000000
+        (doppia-coppia? hand) 2000000
+        (pair? hand) 1000000
+        :else 0))
+
 (defn value-carta-alta
   ""
   [hand]
-  (map #(biginteger (Math/pow % %2))
-                 '(9 10 11 12 13)
-                 '(1 2 3 4 5)))
+  (let [card-values (sort (map :value hand))
+        weights (map #(Math/pow 14 %) (range 0 5))]
+    (int (reduce + (map * card-values weights)))))
+
+(defn value-pair
+  "Returns numerical value of pair in HAND."
+  [hand]
+  (let [card-values (sort (distinct (map :value hand)))
+        weights (map #(Math/pow 14 %) (range 0 4))]
+    (int (+ (reduce + (map * card-values weights))
+            (value-inter-hand-type hand)))))
+
+(defn value-two-pair
+  "Returns numerical value of two-pair in HAND."
+  [hand]
+  (let [card-values (sort (distinct (map :value hand)))
+        weights (map #(Math/pow 14 %) (range 0 3))]
+    (int (+ (reduce + (map * card-values weights))
+            (value-inter-hand-type hand)))))
+
+(defn value-tris
+  "Returns numerical value of tris in HAND"
+  [hand]
+  (let [card-values (sort (map :value hand))]
+    (int (+ (value-inter-hand-type hand)
+            (nth card-values 2)))))
+
+(defn value-full
+  "Returns numerical value of full in HAND"
+  [hand]
+  (value-tris hand))
+
+(defn value-poker
+  "Returns numerical value of poker in HAND"
+  [hand]
+  (value-tris hand))
+
+(defn value-colore
+  "Returns numerical value of colore in HAND"
+  [hand]
+  (int (+ (value-inter-hand-type hand)
+          (value-carta-alta hand))))
+
+(defn value-scala-reale
+  "Returns numerical value of poker in HAND"
+  [hand]
+  1)
+
+(defn value-scala
+  ""
+  [])
+
+(defn hand-value
+  "Returns value of HAND."
+  [hand]
+  (cond (scala-reale? hand) (value-scala-reale hand)
+        (poker? hand) (value-poker hand)
+        (colore? hand) (value-colore hand)
+        (full? hand) (value-full hand)
+        (scala? hand) (value-scala hand)
+        (tris? hand) (value-tris hand)
+        (doppia-coppia? hand) (value-two-pair hand)
+        (pair? hand) (value-pair hand)
+        (carta-alta? hand) (value-carta-alta hand)))
+  
 
 (def esempio-carta-alta (list (card 14 \C)
                               (card 13 \C)
@@ -218,6 +283,12 @@
                           (card 14 \C)
                           (card 12 \F)
                           (card 10 \C)
+                          (card 9 \C)))
+
+(def esempio-doppia-coppia (list (card 14 \C)
+                          (card 14 \C)
+                          (card 12 \F)
+                          (card 12 \C)
                           (card 9 \C)))
 
 (def esempio-tris (list (card 14 \C)
@@ -244,7 +315,7 @@
                          (card 11 \C)
                          (card 10 \C)))
 
-(def esempio-scala-colore (list (card 14 \C)
+(def esempio-scala-reale (list (card 14 \C)
                           (card 13 \C)
                           (card 12 \C)
                           (card 11 \C)
